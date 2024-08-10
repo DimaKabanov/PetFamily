a26d2d3b22bfc512f392ae9e9ca562d7274abc8b
Dmitry Kabanov <Dkabanov6@gmail.com>
2024-08-10 20:54:19 +0500

    ID-B-2/created pet model

---
+++ PetFamily.Backend/src/PetFamily.Domain/Models/Pet.cs
@@ -0,0 +1,40 @@
+﻿using PetFamily.Domain.Enums;
+
+namespace PetFamily.Domain.Models;
+
+public class Pet
+{
+    public Guid Id { get; private set; }
+    
+    public string Name { get; private set; } = default!;
+    
+    public string Type { get; private set; } = default!;
+    
+    public string Description { get; private set; } = default!;
+    
+    public string Breed { get; private set; } = default!;
+    
+    public string Color { get; private set; } = default!;
+    
+    public string Health { get; private set; } = default!;
+    
+    public string Address { get; private set; } = default!;
+    
+    public double Weight { get; private set; }
+    
+    public double Height { get; private set; }
+    
+    public string Phone { get; private set; } = default!;
+    
+    public bool IsCastrated { get; private set; }
+    
+    public DateOnly DateOfBirth { get; private set; }
+    
+    public bool IsVaccinated { get; private set; }
+    
+    public AssistanceStatus AssistanceStatus { get; private set; }
+    
+    public List<Requisite> Requisite { get; private set; } = [];
+
+    public DateTime CreatedDate { get; private set; }
+}
---
+++ PetFamily.Backend/src/PetFamily.Domain/Models/Requisite.cs
@@ -0,0 +1,8 @@
+namespace PetFamily.Domain.Models;
+
+public class Requisite
+{
+    public string Name { get; private set; } = default!;
+    
+    public string Description { get; private set; } = default!;
+}
