67bcde3b69d9c1a37c49e1a00eae9d1f3e29c62c
Dmitry Kabanov <dkabanov6@gmail.com>
2024-08-17 16:35:16 +0500

    ID-B-4/created photo model

---
+++ PetFamily.Backend/src/PetFamily.Domain/Models/PetPhoto.cs
@@ -0,0 +1,10 @@
+namespace PetFamily.Domain.Models;
+
+public class PetPhoto
+{
+    public Guid Id { get; private set; }
+
+    public string Path { get; private set; } = default!;
+
+    public bool IsMain { get; private set; }
+}
