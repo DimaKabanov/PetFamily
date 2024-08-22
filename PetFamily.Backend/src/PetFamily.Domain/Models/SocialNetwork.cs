267b4d610d70dde47cd6a8f5ddef6b5d025854e0
Dmitry Kabanov <Dkabanov6@gmail.com>
2024-08-22 21:03:44 +0500

    ID-B-4-1/added database settings

--- PetFamily.Backend/src/PetFamily.Domain/Models/SocialNetwork.cs
+++ PetFamily.Backend/src/PetFamily.Domain/Models/SocialNetwork.cs
@@ -2,6 +2,8 @@ namespace PetFamily.Domain.Models;
 
 public class SocialNetwork
 {
+    public Guid Id { get; private set; }
+    
     public string Title { get; private set; } = default!;
 
     public string Url { get; private set; } = default!;
---
+++ PetFamily.Backend/src/PetFamily.Infrastructure/Configurations/VolunteerConfiguration.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetFamily.Domain.Models;
+using PetFamily.Domain.Shared;
+
+namespace PetFamily.Infrastructure.Configurations;
+
+public class VolunteerConfiguration : IEntityTypeConfiguration<Volunteer>
+{
+    public void Configure(EntityTypeBuilder<Volunteer> b)
+    {
+        b.ToTable("volunteers");
+
+        b.HasKey(v => v.Id);
+
+        b.Property(v => v.FullName)
+            .IsRequired()
+            .HasMaxLength(Constants.MAX_LOW_TEXT_LENGTH);
+        
+        b.Property(v => v.Description)
+            .IsRequired()
+            .HasMaxLength(Constants.MAX_HIGH_TEXT_LENGTH);
+        
+        b.Property(v => v.Experience)
+            .IsRequired();
+        
+        b.Property(v => v.Phone)
+            .IsRequired()
+            .HasMaxLength(Constants.MAX_LOW_TEXT_LENGTH);
+        
+        b.HasMany(v => v.SocialNetworks)
+            .WithOne()
+            .HasForeignKey("volunteer_id");
+
+        b.HasMany(v => v.Requisites)
+            .WithOne()
+            .HasForeignKey("volunteer_id");
+        
+        b.HasMany(v => v.Pets)
+            .WithOne()
+            .HasForeignKey("volunteer_id");
+    }
+}
