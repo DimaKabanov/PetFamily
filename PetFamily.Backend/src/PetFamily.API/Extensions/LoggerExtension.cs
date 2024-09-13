ce3a9c3d47660dc75f28d03a2902efcd8a7b46e3
Dmitry Kabanov <Dkabanov6@gmail.com>
2024-09-13 07:18:16 +0500

    ID-B-5-5/added exceptions handlers

---
+++ PetFamily.Backend/src/PetFamily.API/Extensions/LoggerExtension.cs
@@ -0,0 +1,19 @@
+using Serilog;
+using Serilog.Events;
+
+namespace PetFamily.API.Extensions;
+
+public static class LoggerExtension
+{
+    public static void ConfigureAppLogger(this WebApplicationBuilder builder)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.Seq(builder.Configuration.GetConnectionString("SeqPath") 
+                         ?? throw new ArgumentNullException("SeqPath"))
+            .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Warning)
+            .CreateLogger();
+    }
+}
