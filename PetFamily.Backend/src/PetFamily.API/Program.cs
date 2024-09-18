e51a28b5349a219f091656e39c894aa155f069d6
Dmitry Kabanov <dkabanov@skbkontur.ru>
2024-09-18 16:31:01 +0500

    ID-B-7-1/added minio

--- PetFamily.Backend/src/PetFamily.API/Program.cs
+++ PetFamily.Backend/src/PetFamily.API/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSerilog();
 
 builder.Services
-    .AddInfrastructure()
+    .AddInfrastructure(builder.Configuration)
     .AddApplication();
 
 builder.Services.AddFluentValidationAutoValidation(configuration =>
--- PetFamily.Backend/src/PetFamily.Infrastructure/Inject.cs
+++ PetFamily.Backend/src/PetFamily.Infrastructure/Inject.cs
@@ -1,15 +1,32 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Minio;
 using PetFamily.Application.Volunteers;
+using PetFamily.Infrastructure.Options;
 using PetFamily.Infrastructure.Repositories;
 
 namespace PetFamily.Infrastructure;
 
 public static class Inject
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
     {
         services.AddScoped<ApplicationDbContext>();
         services.AddScoped<IVolunteersRepository, VolunteersRepository>();
+
+        services.Configure<MinioOptions>(configuration.GetSection(MinioOptions.MINIO));
+
+        services.AddMinio(options =>
+        {
+            var minioOptions = configuration.GetSection(MinioOptions.MINIO).Get<MinioOptions>()
+                ?? throw new ApplicationException("Empty minio configuration");
+            
+            options.WithEndpoint(minioOptions.Endpoint);
+            options.WithCredentials(minioOptions.AccessKey, minioOptions.SecretKey);
+            options.WithSSL(false);
+        });
 
         return services;
     }
