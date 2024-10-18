3e4857a53c1a05626ec644111530ce74e5352412
Dmitry Kabanov <dkabanov@skbkontur.ru>
2024-10-18 16:09:29 +0500

    ID-B-14/added getting pets

--- PetFamily.Backend/src/PetFamily.Application/Volunteers/Queries/GetPets/GetPetsQuery.cs
+++ PetFamily.Backend/src/PetFamily.Application/Volunteers/Queries/GetPets/GetPetsQuery.cs
@@ -2,4 +2,11 @@ using PetFamily.Application.Abstractions;
 
 namespace PetFamily.Application.Volunteers.Queries.GetPets;
 
-public record GetPetsQuery(int Page, int PageSize) : IQuery;
+public record GetPetsQuery(
+    int Page,
+    int PageSize,
+    Guid? VolunteerId,
+    string? Name,
+    string? Description,
+    string? SortBy,
+    string? SortDirection) : IQuery;
